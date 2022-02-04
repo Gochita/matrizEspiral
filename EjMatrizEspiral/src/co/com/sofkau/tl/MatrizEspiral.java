8d76232b627a538bf25f992bc100f4730be0411d
wil <whenao86@gmail.com>
2022-02-04 17:31:50 -0500

    Test de una secuencia

--- EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
+++ EjMatrizEspiral/src/co/com/sofkau/tl/MatrizEspiral.java
@@ -12,6 +12,11 @@ public class MatrizEspiral {
     // TODO: 2/02/2022 método para integrar todas las funcionalidades anteriores por medio de un menú que se comunique con el usuario
 
     public static int[][] matriz;
+    /*
+    controlArray[0]: contiene posicion en la que termina el metodo
+    controlArray[1]: contiene contador con que termina el metodo
+     */
+    public static int[] controlArray = new int[2];
 
     public static void main(String[] args) {
 
@@ -20,18 +25,21 @@ public class MatrizEspiral {
         int valIn= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese valor inicial"));
         int puntoInicio= Integer.parseInt(JOptionPane.showInputDialog(null, "Elija fila/columna para iniciar"));
         matriz = new int[filas][columnas];
-        recorrerFilaDerechaIzquerda(matriz, puntoInicio,valIn);
-        imprimirMatriz(matriz);
+        recorrerFilaIzquierdaDerecha(puntoInicio,valIn);
+        recorrerColumnaDescendente(controlArray[0],controlArray[1]);
+        recorrerFilaDerechaIzquerda(controlArray[0],controlArray[1]);
+        imprimirMatriz();
 
     }
     // parámetros: matriz, nro fila o columna, el nro en el que comienza
 
 
-    private static void imprimirMatriz(int[][] matriz) {
+    private static void imprimirMatriz() {
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
+                System.out.print(matriz[i][j] + "  " +
+                        "");
             }
             System.out.println("");
         }
@@ -39,33 +47,40 @@ public class MatrizEspiral {
 
     }
 
-    private static void recorrerFilaIzquierdaDerecha(int[][] matriz, int numeroFila, int valorInicial) {
+    private static void recorrerFilaIzquierdaDerecha(int numeroFila, int valorInicial) {
         int cont = valorInicial;
         for (int i = 0; i < matriz[numeroFila-1].length; i++) {
             if (matriz[numeroFila-1][i] == 0) {
                 matriz[numeroFila-1][i] = cont;
                 cont++;
+                controlArray[0] = i+1;
+                controlArray[1] = cont;
             }
         }
     }
 
-    private static void recorrerFilaDerechaIzquerda(int[][] matriz, int numeroFila, int valorInicial) {
+    private static void recorrerFilaDerechaIzquerda(int numeroFila, int valorInicial) {
         int contador = valorInicial;
         for (int i = matriz[numeroFila-1].length-1; i >= 0; i--) {
             if (matriz[numeroFila-1][i] == 0) {
                 matriz[numeroFila-1][i] = contador;
                 contador++;
+                controlArray[0] = i+1;
+                controlArray[1] = contador;
+
             }
         }
 
     }
 
-    private static void recorrerColumnaDescendente(int[][] matriz, int numeroColumna, int valorInicial) {
+    private static void recorrerColumnaDescendente(int numeroColumna, int valorInicial) {
         int contador = valorInicial;
         for (int i = 0; i < matriz.length; i++) {
             if (matriz[i][numeroColumna - 1] == 0) {
                 matriz[i][numeroColumna - 1] = contador;
                 contador++;
+                controlArray[0] = i+1;
+                controlArray[1] = contador;
             }
         }
     }
